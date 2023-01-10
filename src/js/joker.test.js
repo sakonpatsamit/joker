3fc2fccb80e262f52baf40dcb7d854a14514c01f
sakonpatsamit <95245596+sakonpatsamit@users.noreply.github.com>
2023-01-10 08:43:36 +0100

    added a variable

--- src/js/joker.test.js
+++ src/js/joker.test.js
@@ -1,11 +1,23 @@
-import { dadJoke } from "./joker.js";
+import { dadJoke } from './joker.js';
 
 export async function testDadJoke() {
   const joke = await dadJoke();
   const testPasses = Boolean(joke);
   console.log(joke);
-  console.log("Dad Joke Test:", testPasses ? "PASSED" : "FAILED");
+  console.log('Dad Joke Test:', testPasses ? 'PASSED' : 'FAILED');
   return testPasses;
 }
 
 testDadJoke();
+
+export function dadJoke() {
+  return new Promise(function (resolve, reject) {
+    try {
+      joker.getRandomDadJoke(function (joke) {
+        resolve(joke);
+      });
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
